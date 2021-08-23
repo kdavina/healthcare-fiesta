d36f86625d0d20b6268b097d0f41d9475f040903
Davina Kam <kamdavina@gmail.com>
2021-08-22 23:24:50 -0600

    Generate dynamic table based on checked boxes

--- utils.js
+++ utils.js
@@ -1,20 +1,74 @@
 function homepageSetup(){
     d3.csv("Drug-List.csv").get(
         function(data){
-            console.log(data)
-            createCheckboxes(data.map(data => data.GenericName)) // mapping: https://stackoverflow.com/questions/19590865/from-an-array-of-objects-extract-value-of-a-property-as-array
+            drugArray = data;
+            // console.log("raw data", data);
+            // console.log("drugArray", drugArray);
+            createCheckboxes(data.map(data => data.GenericName)); // mapping: https://stackoverflow.com/questions/19590865/from-an-array-of-objects-extract-value-of-a-property-as-array
     })
+    
 }
 
-let createCheckboxes = function(items){
+function createCheckboxes(items){
     for (i = 0; i < items.length; i++){
         // https://stackoverflow.com/questions/28678027/create-and-populate-with-dom-a-checkbox-list-with-array-values-in-javascript/28678424
         let checkBox = document.createElement("input");
         let label = document.createElement("label");
         checkBox.type = "checkbox";
         checkBox.value = items[i];
+        checkBox.name = "GenericName";
         drugList.appendChild(checkBox);
         drugList.appendChild(label);
         label.appendChild(document.createTextNode(items[i]));
     };
+}
+
+function generateTable(){
+    d3.csv("Drug-List.csv").get(
+        function(data){
+            let drugArray = data;
+            // https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces
+            let body = document.getElementsByTagName("body")[0];
+            let table = document.createElement("table");
+            let tBody = document.createElement("tbody"); 
+            // console.log("header names", d3.keys(data[0])); // https://stackoverflow.com/questions/20456232/d3-js-is-there-a-way-to-access-csv-header-values-into-a-dropdown
+            let headerRow = document.createElement("tr");
+            let headerData = d3.keys(data[0]);
+            for (i=0; i< headerData.length; i++){
+                let cell = document.createElement("th");
+                // console.log("headerData[i]", headerData[i]);
+                let cellText = document.createTextNode(headerData[i]);
+                cell.appendChild(cellText);
+                headerRow.appendChild(cell);
+            }
+            tBody.appendChild(headerRow);
+            let checkedDrugs = document.getElementsByName("GenericName");
+            for (drugName of checkedDrugs){
+                if (drugName.checked){
+                    let row = document.createElement("tr");
+                    let fetchedGenericName = drugName.value;
+                    const drugData = Object.values(data.find(element => element.GenericName === drugName.value));
+                    console.log("drugData", drugData);
+                    // console.log("parsing individual drug data", d3.keys(drugData));
+                    // console.log("drugData.length", drugData.length);
+                    for (i=0; i < drugData.length; i++){
+                        console.log("entered row")
+                        let cell = document.createElement("td");
+                        console.log("drugData[i]", drugData[i]);
+                        let cellText = document.createTextNode(drugData[i]);
+                        cell.appendChild(cellText);
+                        row.appendChild(cell);
+                        console.log("cellText", cellText);
+                    }
+                    tBody.appendChild(row);
+                }   
+            }
+
+            // https://www.javatpoint.com/how-to-get-all-checked-checkbox-value-in-javascript
+            // console.log("checked boxes", document.getElementsByName("GenericName"));
+
+
+            table.appendChild(tBody);
+            body.appendChild(table);
+    })
 }
